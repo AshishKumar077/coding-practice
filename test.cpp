2f10f041895a3c5bcc59e8bf1e2916ff60a256be
AshishKumar077 <72354811+AshishKumar077@users.noreply.github.com>
2021-09-30 21:18:23 +0530

    Create test.cpp

---
+++ test.cpp
@@ -0,0 +1,15 @@
+  #include<bits/stdc++.h>
+  using namespace std;
+
+int main() {
+    int t,i;
+    cin>>t;
+    for(i=0;i<t;i++){
+	int a,b,c;
+	cin>>a;
+	cin>>b;
+	c=a%b;
+	cout<<c<<endl;
+    }
+	return 0;
+}
